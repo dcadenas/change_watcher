4542bc4b06050534541d7bdb4c82d70daa7cd641
Daniel Cadenas <dcadenas@gmail.com>
2009-06-13 14:25:22 -0300

    Initial commit to change_watcher.

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'change_watcher'
+
+class Test::Unit::TestCase
+end
